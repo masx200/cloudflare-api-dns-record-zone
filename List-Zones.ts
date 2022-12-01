9038419e0d4b8b17f364b588302452719d71b074
GitHub <noreply@github.com>
2022-12-01 19:45:37 +0000

    Update dependencies

--- List-Zones.ts
+++ List-Zones.ts
@@ -8,7 +8,7 @@ import {
 } from "./cloudflare-DNS-Records-for-a-Zone.ts";
 import { set_url_search_params } from "./set_url_search_params.ts";
 import { Zone } from "./Zone.ts";
-import { assert } from "https://deno.land/std@0.166.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 
 export type ListZoneOptions = AuthorizationOptions & {
     parameters?:
